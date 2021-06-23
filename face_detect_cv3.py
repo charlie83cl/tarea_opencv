751741c41c8585b28cc1a173d05b07f3a69328f5
charlie83cl <82404370+charlie83cl@users.noreply.github.com>
2021-06-23 12:36:25 -0400

    Enviando la tarea de Opencv estuvo de lujo se lucio el Nico enteroseco. subo mis archivos en otros los modifique y funcionan directos sin ambientes virtuales. pero no sirven como prueba sino como ejemplo

---
+++ face_detect_cv3.py
@@ -0,0 +1,34 @@
+import cv2
+import sys
+
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.2,
+    minNeighbors=3,
+    minSize=(30, 30)
+    #flags = cv2.CV_HAAR_SCALE_IMAGE
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
+
+#EJECUTAR ESTE COMANDO EN EL TERMINAL PARA ABRIR EL DETECTOR DE IMAGEN
+#python face_detect_cv3.py nosoyyo.jpg
